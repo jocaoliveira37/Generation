221dc76931f8ff8069779e672cca9ed4d749df0c
Joca <jocajuni@gmail.com>
2020-11-12 15:30:00 -0300

    Exercicios iniciais de Java

---
+++ Semana 3/Java/course1/src/course1/main.java
@@ -0,0 +1,13 @@
+package course1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String habilidadesEMentalidades = "Utilizou-se as mentalidades de  crescimento e persistência para a conseguir \n"
+				+ "produzir esses programas em java, as habilidades foram de atenção ao detalhe e proatividade.";
+		System.out.println(habilidadesEMentalidades);
+	}
+	
+}
