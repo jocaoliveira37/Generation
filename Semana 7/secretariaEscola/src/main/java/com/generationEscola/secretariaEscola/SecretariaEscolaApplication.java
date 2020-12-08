9289ff840b0b33b5a44bf5fc586cf9d6f6a8f52d
Joca <jocajuni@gmail.com>
2020-12-08 18:11:52 -0300

    Exercicios de CRUD 08/12

---
+++ Semana 7/secretariaEscola/src/main/java/com/generationEscola/secretariaEscola/SecretariaEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.generationEscola.secretariaEscola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariaEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariaEscolaApplication.class, args);
+	}
+
+}
---
+++ Semana 7/secretariaEscola/src/test/java/com/generationEscola/secretariaEscola/SecretariaEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generationEscola.secretariaEscola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretariaEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
